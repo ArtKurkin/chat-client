ef63db08b86c2572ae1343efecce45b33d6364f3
ArtKurkin <art.curkin@gmail.com>
2023-04-21 21:55:41 +0300

    Add autoConnect

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import io from "socket.io-client";
 
-const socket = io("https://online-chat-server-eisw.onrender.com");
+const socket = io("https://online-chat-server-eisw.onrender.com", {
+  transports: ["websocket"],
+  autoConnect: true,
+});
 
 export default socket;
