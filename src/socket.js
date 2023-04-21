a4d93564438f55b5a4fd37a763b6afebc7101b16
ArtKurkin <art.curkin@gmail.com>
2023-04-21 22:28:52 +0300

    reconnect

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,18 @@ import io from "socket.io-client";
 
 const socket = io("https://online-chat-server-eisw.onrender.com", {
   transports: ["websocket"],
-  autoConnect: true,
 });
+
+const tryReconnect = () => {
+  setTimeout(() => {
+    socket.io.open((err) => {
+      if (err) {
+        tryReconnect();
+      }
+    });
+  }, 2000);
+};
+
+socket.io.on("close", tryReconnect);
 
 export default socket;
